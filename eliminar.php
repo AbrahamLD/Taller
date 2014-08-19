e022ccd963c9e1e6da3a0f0c80d605b755750e82
AbrahamLD <hablaloco@gmail.com>
2014-08-19 12:24:34 -0500

    Create eliminar.php

---
+++ eliminar.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+</head>
+
+<body>
+<div id="main">
+<?php
+//obtener el valor de ID que viene del metodo GET a traves de HTTP
+$id=$_GET["id"];
+
+//incluir la libreria de DemoCollector
+
+include_once("comedoresControlador.php");
+//creo una instancia de DemoCollector
+$DemoCollectorObj = new ComedorControlador();
+//Ejecuto el metodo para eliminar el objeto Demo indicando el id
+$DemoCollectorObj->eliminarComedor($id);
+
+// muestro mensaje de que se ha eliminado el objeto Demo
+echo "valor id". htmlspecialchars($id) ."  ha sido eliminado correctamente.";
+?>
+<div><a href="index.php">Volver al Inicio</a></div>
+</div>
+</body>
+</html>
